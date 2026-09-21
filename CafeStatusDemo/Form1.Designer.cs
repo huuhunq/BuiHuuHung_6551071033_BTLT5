91e997683012de233423872581c73123cb8d55fa
Bùi Hữu Hùng <hungbui12345h@gmail.com>
2026-09-21 11:06:28 +0700

    Add files via upload

---
+++ CafeStatusDemo/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CafeStatusDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            menuStrip1 = new MenuStrip();
+            hệThốngToolStripMenuItem = new ToolStripMenuItem();
+            đổiMàuNềnToolStripMenuItem = new ToolStripMenuItem();
+            thoátToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1 = new StatusStrip();
+            lblGioHienTai = new ToolStripStatusLabel();
+            lblTenQuan = new ToolStripStatusLabel();
+            lblTrangThai = new ToolStripStatusLabel();
+            timer1 = new System.Windows.Forms.Timer(components);
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { hệThốngToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // hệThốngToolStripMenuItem
+            // 
+            hệThốngToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { đổiMàuNềnToolStripMenuItem, thoátToolStripMenuItem });
+            hệThốngToolStripMenuItem.Name = "hệThốngToolStripMenuItem";
+            hệThốngToolStripMenuItem.Size = new Size(88, 24);
+            hệThốngToolStripMenuItem.Text = "Hệ Thống";
+            // 
+            // đổiMàuNềnToolStripMenuItem
+            // 
+            đổiMàuNềnToolStripMenuItem.Name = "đổiMàuNềnToolStripMenuItem";
+            đổiMàuNềnToolStripMenuItem.Size = new Size(224, 26);
+            đổiMàuNềnToolStripMenuItem.Text = "Đổi Màu Nền";
+            đổiMàuNềnToolStripMenuItem.Click += đổiMàuNềnToolStripMenuItem_Click;
+            // 
+            // thoátToolStripMenuItem
+            // 
+            thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            thoátToolStripMenuItem.Size = new Size(224, 26);
+            thoátToolStripMenuItem.Text = "Thoát";
+            thoátToolStripMenuItem.Click += thoátToolStripMenuItem_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lblGioHienTai, lblTenQuan, lblTrangThai });
+            statusStrip1.Location = new Point(0, 424);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 26);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lblGioHienTai
+            // 
+            lblGioHienTai.Name = "lblGioHienTai";
+            lblGioHienTai.Size = new Size(0, 20);
+            lblGioHienTai.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // lblTenQuan
+            // 
+            lblTenQuan.Name = "lblTenQuan";
+            lblTenQuan.Size = new Size(785, 20);
+            lblTenQuan.Spring = true;
+            lblTenQuan.Text = "CAFE ÁNH DƯƠNG";
+            // 
+            // lblTrangThai
+            // 
+            lblTrangThai.Name = "lblTrangThai";
+            lblTrangThai.Size = new Size(0, 20);
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem hệThốngToolStripMenuItem;
+        private ToolStripMenuItem đổiMàuNềnToolStripMenuItem;
+        private ToolStripMenuItem thoátToolStripMenuItem;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lblGioHienTai;
+        private ToolStripStatusLabel lblTenQuan;
+        private ToolStripStatusLabel lblTrangThai;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ CafeStatusDemo/Form1.cs
@@ -0,0 +1,46 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CafeStatusDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+
+            lblGioHienTai.Text = now.ToString("HH:mm:ss");
+
+            if (now.Hour >= 6 && now.Hour < 22)
+            {
+                lblTrangThai.Text = "Đang mở cửa";
+                lblTrangThai.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblTrangThai.Text = "Đã đóng cửa";
+                lblTrangThai.ForeColor = Color.Red;
+            }
+        }
+
+        private void đổiMàuNềnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.BackColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
