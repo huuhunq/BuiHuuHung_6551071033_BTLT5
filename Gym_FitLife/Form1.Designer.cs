c46de65ab93bc37d9effb02ce8945f06c9b07c32
Bùi Hữu Hùng <hungbui12345h@gmail.com>
2026-09-21 11:08:07 +0700

    Add files via upload

---
+++ Gym_FitLife/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Gym_FitLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            txtHoTen = new TextBox();
+            txtSDT = new TextBox();
+            txtEmail = new TextBox();
+            dtpNgaySinh = new DateTimePicker();
+            cboGoiTap = new ComboBox();
+            numSoBuoiTuan = new NumericUpDown();
+            btnDangKy = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            toolTip1 = new ToolTip(components);
+            ((System.ComponentModel.ISupportInitialize)numSoBuoiTuan).BeginInit();
+            SuspendLayout();
+            // 
+            // txtHoTen
+            // 
+            txtHoTen.Location = new Point(230, 49);
+            txtHoTen.Name = "txtHoTen";
+            txtHoTen.Size = new Size(125, 27);
+            txtHoTen.TabIndex = 0;
+            toolTip1.SetToolTip(txtHoTen, "Nhập đầy đủ họ và tên");
+            txtHoTen.TextChanged += txtHoTen_TextChanged;
+            // 
+            // txtSDT
+            // 
+            txtSDT.Location = new Point(230, 108);
+            txtSDT.Name = "txtSDT";
+            txtSDT.Size = new Size(125, 27);
+            txtSDT.TabIndex = 1;
+            toolTip1.SetToolTip(txtSDT, "Nhập đúng 10 chữ số, không chứa khoảng trắng hay ký tự đặc biệt");
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(230, 175);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(125, 27);
+            txtEmail.TabIndex = 2;
+            toolTip1.SetToolTip(txtEmail, "Email dùng để nhận thông báo lịch tập và khuyến mãi");
+            // 
+            // dtpNgaySinh
+            // 
+            dtpNgaySinh.Location = new Point(230, 240);
+            dtpNgaySinh.Name = "dtpNgaySinh";
+            dtpNgaySinh.Size = new Size(216, 27);
+            dtpNgaySinh.TabIndex = 3;
+            toolTip1.SetToolTip(dtpNgaySinh, "Chọn ngày tháng năm sinh");
+            // 
+            // cboGoiTap
+            // 
+            cboGoiTap.FormattingEnabled = true;
+            cboGoiTap.Items.AddRange(new object[] { "Basic", "VIP", "Premium" });
+            cboGoiTap.Location = new Point(570, 107);
+            cboGoiTap.Name = "cboGoiTap";
+            cboGoiTap.Size = new Size(151, 28);
+            cboGoiTap.TabIndex = 4;
+            toolTip1.SetToolTip(cboGoiTap, "Gói VIP và Premium có kèm ");
+            // 
+            // numSoBuoiTuan
+            // 
+            numSoBuoiTuan.Location = new Point(571, 52);
+            numSoBuoiTuan.Maximum = new decimal(new int[] { 7, 0, 0, 0 });
+            numSoBuoiTuan.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numSoBuoiTuan.Name = "numSoBuoiTuan";
+            numSoBuoiTuan.Size = new Size(150, 27);
+            numSoBuoiTuan.TabIndex = 5;
+            toolTip1.SetToolTip(numSoBuoiTuan, "Chọn số buổi tập trong tuần, từ 1 đến 7");
+            numSoBuoiTuan.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // btnDangKy
+            // 
+            btnDangKy.Location = new Point(352, 382);
+            btnDangKy.Name = "btnDangKy";
+            btnDangKy.Size = new Size(94, 29);
+            btnDangKy.TabIndex = 6;
+            btnDangKy.Text = "Đăng kí";
+            toolTip1.SetToolTip(btnDangKy, "Nhấn để đăng ký gói tập");
+            btnDangKy.UseVisualStyleBackColor = true;
+            btnDangKy.Click += btnDangKy_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(131, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Họ và tên:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(131, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 20);
+            label2.TabIndex = 8;
+            label2.Text = "SDT:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(131, 182);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(131, 247);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Ngày Sinh:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(463, 56);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Số buổi tập:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(463, 115);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 20);
+            label6.TabIndex = 12;
+            label6.Text = "Gói tập:";
+            // 
+            // toolTip1
+            // 
+            toolTip1.ShowAlways = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnDangKy);
+            Controls.Add(numSoBuoiTuan);
+            Controls.Add(cboGoiTap);
+            Controls.Add(dtpNgaySinh);
+            Controls.Add(txtEmail);
+            Controls.Add(txtSDT);
+            Controls.Add(txtHoTen);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numSoBuoiTuan).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtHoTen;
+        private TextBox txtSDT;
+        private TextBox txtEmail;
+        private DateTimePicker dtpNgaySinh;
+        private ComboBox cboGoiTap;
+        private NumericUpDown numSoBuoiTuan;
+        private Button btnDangKy;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ToolTip toolTip1;
+    }
+}
---
+++ Gym_FitLife/Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace Gym_FitLife
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHoTen_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtHoTen.Text) ||
+        string.IsNullOrWhiteSpace(txtSDT.Text))
+            {
+                MessageBox.Show(
+                    "Vui lòng nhập đầy đủ Họ tên và Số điện thoại",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+            }
+            else
+            {
+                string hoTen = txtHoTen.Text;
+                string sdt = txtSDT.Text;
+                string goiTap = cboGoiTap.Text;
+                string soBuoiTuan = numSoBuoiTuan.Value.ToString();
+
+                MessageBox.Show(
+                    "THÔNG TIN ĐĂNG KÝ\n\n" +
+                    "Họ tên: " + hoTen + "\n" +
+                    "SĐT: " + sdt + "\n" +
+                    "Gói tập: " + goiTap + "\n" +
+                    "Số buổi/tuần: " + soBuoiTuan,
+                    "Đăng ký thành công",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+        }
+    }
+}
