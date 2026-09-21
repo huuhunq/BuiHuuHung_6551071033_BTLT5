d01df7e5af3d283c7956abb917069c83bee90e46
Bùi Hữu Hùng <hungbui12345h@gmail.com>
2026-09-21 11:11:00 +0700

    Add files via upload

---
+++ QuanLyThoiGian/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace QuanLyThoiGian
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            txtCongViecMoi = new TextBox();
+            btnThem = new Button();
+            lstCongViec = new ListBox();
+            cmsCongViec = new ContextMenuStrip(components);
+            đánhDấuHoànThànhToolStripMenuItem = new ToolStripMenuItem();
+            xóaCôngViệcNàyToolStripMenuItem = new ToolStripMenuItem();
+            xóaTấtCảToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            cmsCongViec.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtCongViecMoi
+            // 
+            txtCongViecMoi.Location = new Point(174, 59);
+            txtCongViecMoi.Name = "txtCongViecMoi";
+            txtCongViecMoi.Size = new Size(125, 27);
+            txtCongViecMoi.TabIndex = 0;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(369, 57);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(94, 29);
+            btnThem.TabIndex = 1;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // lstCongViec
+            // 
+            lstCongViec.ContextMenuStrip = cmsCongViec;
+            lstCongViec.Dock = DockStyle.Right;
+            lstCongViec.FormattingEnabled = true;
+            lstCongViec.Location = new Point(650, 0);
+            lstCongViec.Name = "lstCongViec";
+            lstCongViec.Size = new Size(150, 450);
+            lstCongViec.TabIndex = 2;
+            lstCongViec.SelectedIndexChanged += lstCongViec_SelectedIndexChanged;
+            // 
+            // cmsCongViec
+            // 
+            cmsCongViec.ImageScalingSize = new Size(20, 20);
+            cmsCongViec.Items.AddRange(new ToolStripItem[] { đánhDấuHoànThànhToolStripMenuItem, xóaCôngViệcNàyToolStripMenuItem, xóaTấtCảToolStripMenuItem });
+            cmsCongViec.Name = "cmsCongViec";
+            cmsCongViec.Size = new Size(221, 76);
+            // 
+            // đánhDấuHoànThànhToolStripMenuItem
+            // 
+            đánhDấuHoànThànhToolStripMenuItem.Name = "đánhDấuHoànThànhToolStripMenuItem";
+            đánhDấuHoànThànhToolStripMenuItem.Size = new Size(220, 24);
+            đánhDấuHoànThànhToolStripMenuItem.Text = "Đánh dấu hoàn thành";
+            đánhDấuHoànThànhToolStripMenuItem.Click += đánhDấuHoànThànhToolStripMenuItem_Click;
+            // 
+            // xóaCôngViệcNàyToolStripMenuItem
+            // 
+            xóaCôngViệcNàyToolStripMenuItem.Name = "xóaCôngViệcNàyToolStripMenuItem";
+            xóaCôngViệcNàyToolStripMenuItem.Size = new Size(220, 24);
+            xóaCôngViệcNàyToolStripMenuItem.Text = "Xóa công việc này";
+            xóaCôngViệcNàyToolStripMenuItem.Click += xóaCôngViệcNàyToolStripMenuItem_Click;
+            // 
+            // xóaTấtCảToolStripMenuItem
+            // 
+            xóaTấtCảToolStripMenuItem.Name = "xóaTấtCảToolStripMenuItem";
+            xóaTấtCảToolStripMenuItem.Size = new Size(220, 24);
+            xóaTấtCảToolStripMenuItem.Text = "Xóa tất cả";
+            xóaTấtCảToolStripMenuItem.Click += xóaTấtCảToolStripMenuItem_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Công Việc";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(lstCongViec);
+            Controls.Add(btnThem);
+            Controls.Add(txtCongViecMoi);
+            Name = "Form1";
+            Text = "Form1";
+            cmsCongViec.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtCongViecMoi;
+        private Button btnThem;
+        private ListBox lstCongViec;
+        private ContextMenuStrip cmsCongViec;
+        private ToolStripMenuItem đánhDấuHoànThànhToolStripMenuItem;
+        private ToolStripMenuItem xóaCôngViệcNàyToolStripMenuItem;
+        private ToolStripMenuItem xóaTấtCảToolStripMenuItem;
+        private Label label1;
+    }
+}
---
+++ QuanLyThoiGian/Form1.cs
@@ -0,0 +1,74 @@
+﻿namespace QuanLyThoiGian
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lstCongViec_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtCongViecMoi.Text))
+            {
+                lstCongViec.Items.Add(txtCongViecMoi.Text);
+                txtCongViecMoi.Clear();
+                txtCongViecMoi.Focus();
+            }
+        }
+
+        private void đánhDấuHoànThànhToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (lstCongViec.SelectedItem != null)
+            {
+                string congViec = lstCongViec.SelectedItem.ToString();
+
+                if (!congViec.StartsWith("[Hoàn thành] "))
+                {
+                    int viTri = lstCongViec.SelectedIndex;
+
+                    lstCongViec.Items[viTri] = "[Hoàn thành] " + congViec;
+
+                    lstCongViec.SelectedIndex = viTri;
+                }
+            }
+        }
+
+        private void xóaCôngViệcNàyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (lstCongViec.SelectedItem != null)
+            {
+                lstCongViec.Items.Remove(lstCongViec.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Vui lòng chọn một công việc trước khi xóa.",
+                    "Thông báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+            }
+        }
+
+        private void xóaTấtCảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult ketQua = MessageBox.Show(
+                "Bạn có chắc muốn xóa tất cả công việc không?",
+                "Xác nhận xóa",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (ketQua == DialogResult.Yes)
+            {
+                lstCongViec.Items.Clear();
+            }
+        }
+    }
+}
