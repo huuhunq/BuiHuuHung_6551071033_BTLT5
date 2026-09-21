a60762c59d79f54b2f8f757740e14bd0bf18c85f
Bùi Hữu Hùng <hungbui12345h@gmail.com>
2026-09-21 11:16:03 +0700

    Add files via upload

---
+++ BangVeMini/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace BangVeMini
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlCanvas = new Panel();
+            lblViTri = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // pnlCanvas
+            // 
+            pnlCanvas.BackColor = Color.White;
+            pnlCanvas.BackgroundImageLayout = ImageLayout.None;
+            pnlCanvas.Location = new Point(-1, 70);
+            pnlCanvas.Name = "pnlCanvas";
+            pnlCanvas.Size = new Size(645, 323);
+            pnlCanvas.TabIndex = 0;
+            pnlCanvas.Paint += pnlCanvas_Paint;
+            pnlCanvas.MouseClick += pnlCanvas_MouseClick;
+            pnlCanvas.MouseDown += pnlCanvas_MouseDown;
+            pnlCanvas.MouseMove += pnlCanvas_MouseMove;
+            pnlCanvas.MouseUp += pnlCanvas_MouseUp;
+            // 
+            // lblViTri
+            // 
+            lblViTri.AutoSize = true;
+            lblViTri.BackColor = Color.White;
+            lblViTri.Location = new Point(335, 21);
+            lblViTri.Name = "lblViTri";
+            lblViTri.Size = new Size(21, 20);
+            lblViTri.TabIndex = 1;
+            lblViTri.Text = "   ";
+            lblViTri.Click += lblViTri_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Location = new Point(190, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Tọa độ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(lblViTri);
+            Controls.Add(pnlCanvas);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnlCanvas;
+        private Label lblViTri;
+        private Label label1;
+    }
+}
---
+++ BangVeMini/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace BangVeMini
+{
+    public partial class Form1 : Form
+    {
+        private bool dangVe = false;
+        private Point diemCuoi;
+        private Bitmap bitmapVe;
+        private Graphics graphicsVe;
+        public Form1()
+        {
+            InitializeComponent();
+            bitmapVe = new Bitmap(pnlCanvas.Width, pnlCanvas.Height);
+
+            // Tạo Graphics để vẽ lên Bitmap
+            graphicsVe = Graphics.FromImage(bitmapVe);
+
+            // Nền trắng
+            graphicsVe.Clear(Color.White);
+
+            // Đưa Bitmap lên Panel
+            pnlCanvas.BackgroundImage = bitmapVe;
+
+            // Hiển thị trạng thái ban đầu
+            lblViTri.Text = "X: 0, Y: 0 - Sẵn sàng";
+        }
+
+        private void pnlCanvas_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pnlCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dangVe = true;
+
+              
+                diemCuoi = e.Location;
+
+                lblViTri.Text =
+                    $"X: {e.X}, Y: {e.Y} - Đang vẽ...";
+            }
+        }
+
+        private void pnlCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblViTri.Text =
+        $"X: {e.X}, Y: {e.Y}" +
+        (dangVe ? " - Đang vẽ..." : " - Sẵn sàng");
+
+            // Nếu đang giữ chuột trái thì vẽ
+            if (dangVe && e.Button == MouseButtons.Left)
+            {
+                using (Pen pen = new Pen(Color.Black, 2))
+                {
+                    pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                    pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+                    graphicsVe.DrawLine(
+                        pen,
+                        diemCuoi,
+                        e.Location
+                    );
+                }
+
+                
+                diemCuoi = e.Location;
+
+                
+                pnlCanvas.Invalidate();
+            }
+        }
+
+        private void pnlCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dangVe = false;
+
+                lblViTri.Text =
+                    $"X: {e.X}, Y: {e.Y} - Sẵn sàng";
+            }
+        }
+
+        private void pnlCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+               
+                graphicsVe.Clear(Color.White);
+
+                
+                pnlCanvas.Invalidate();
+
+          
+                lblViTri.Text =
+                    $"X: {e.X}, Y: {e.Y} - Sẵn sàng";
+            }
+        }
+
+        private void lblViTri_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
